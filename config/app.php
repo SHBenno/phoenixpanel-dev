d8f9e094443ecfa6779f2caa275760162812caa4
Benno <77901422+BennoMLRP0908@users.noreply.github.com>
2025-03-29 09:22:37 +0000

    Patch #1
    Target class [encrypter] does not exist.

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class, // Added default back
         Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
